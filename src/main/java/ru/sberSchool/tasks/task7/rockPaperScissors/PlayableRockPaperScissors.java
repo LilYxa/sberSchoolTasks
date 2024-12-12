911650cc4946767ed9641549e8059e0e978d4eee
LilYxa <ilia.elland@mail.ru>
2024-12-12 17:42:10 +0300

    Выполнено задание 7.

---
+++ src/main/java/ru/sberSchool/tasks/task7/rockPaperScissors/PlayableRockPaperScissors.java
@@ -0,0 +1,8 @@
+package ru.sberSchool.tasks.task7.rockPaperScissors;
+
+import ru.sberSchool.tasks.task7.rockPaperScissors.enums.RockPaperScissorsEnum;
+
+public interface PlayableRockPaperScissors {
+    RockPaperScissorsEnum play();
+    String getName();
+}
